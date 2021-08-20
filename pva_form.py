19eaf8fadd5f8084d1bd100101a372d467289467
sean-was-taken <lelefacai@gmail.com>
2021-08-20 10:19:28 -0700

    update

--- pva_form.py
+++ pva_form.py
@@ -15,18 +15,26 @@ for x in range(1, len(tmp), 2):
     entries.append(tmp[x])
 print(entries)
 
+# replace the following values with the information you wish to send to the form. 
 responce = {
-    "First Name": "your firstname here",
-    "Last Name": "your lastname here",
-    "Student Number": "student number here",
+    "First Name": "Hello",
+    "Last Name": "World",
+    "Student Number": 666666, 
+    "Grade Level": 7,
+    "ans1": "No", 
+    "ans2": "No" 
 }
 
 parser = {
     "First Name": entries[0],
     "Last Name": entries[1],
-    "Student Number": 205099
+    "Student Number": entries[2],
+    "Grade Level": entries[3],
+    "ans1": entries[4],
+    "ans2": entries[5]
 }
 url = "https://docs.google.com/forms/d/e/1FAIpQLSdBkt1jwgWZgpxhKUdQUM9S3Gn5qzXKJrtW0_HZbPAJaOeo5w/viewform?"
+
 for name, code in parser.items():
     url += f"entry.{code}={responce.get(name)}&"
 
