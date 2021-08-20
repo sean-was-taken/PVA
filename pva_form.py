10b9bc30ced41d270a8f3e4a039a4f27cc633a12
sean-was-taken <86674443+sean-was-taken@users.noreply.github.com>
2021-08-20 09:11:16 -0700

    Update pva_form.py
    edit file

--- pva_form.py
+++ pva_form.py
@@ -1,24 +1,18 @@
-base_url = "https://docs.google.com/forms/d/e/1FAIpQLSdBkt1jwgWZgpxhKUdQUM9S3Gn5qzXKJrtW0_HZbPAJaOeo5w/viewform"
+base_url = "https://docs.google.com/forms/d/e/1FAIpQLSdBkt1jwgWZgpxhKUdQUM9S3Gn5qzXKJrtW0_HZbPAJaOeo5w/viewform?"
 
 responce = {
-    "First Name": "Xinle",
-    "Last Name": "Yao",
-    "Student Number": "109018",
+    "First Name": "your firstname here",
+    "Last Name": "your lastname here",
+    "Student Number": "student number ere",
     "Success": "Yes"
 }
 codes = {
     "First Name": 911369318,
     "Last Name": 1500907496,
-    "Student Number": 2050994802,
-    "Success": 243311362
+    "Student Number": 2050994802
 }
-append_url = "?"
 for name, code in codes.items():
-    append_url += "entry."
-    append_url += str(code)
-    append_url += "="
-    append_url += responce.get(name)
-    append_url += "&"
+    base_url += f"entry.{code}={responce.get(name)}&"
 
-print(base_url + append_url)
+print(base_url)
 
