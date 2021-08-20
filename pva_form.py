4f862234e360e59805fd74bd98f08c3ad32e0e7c
sean-was-taken <lelefacai@gmail.com>
2021-08-20 10:30:23 -0700

    update

--- pva_form.py
+++ pva_form.py
@@ -1,19 +1,4 @@
-import re
-
-# read entries
-with open("entries.txt", "r") as d:
-    entries = d.read()
-
-# preprocessing
-tmp = re.findall(f'\d+', entries)
-tmp = list(map(int, tmp))
-tmp = list(filter(lambda i: i > 1000 and i < 1000000000000000, tmp))
-entries = []
-
-# extract entries
-for x in range(1, len(tmp), 2):
-    entries.append(tmp[x])
-print(entries)
+#--------- settings --------#
 
 # replace the following values with the information you wish to send to the form. 
 responce = {
@@ -25,6 +10,27 @@ responce = {
     "ans2": "No" 
 }
 
+# use autosubmit = True for auto submit
+autosubmit = False
+
+#-------- settings --------#
+
+import re
+
+# read entries
+with open("entries.txt", "r") as d:
+    entries = d.read()
+
+# preprocessing
+tmp = re.findall(f'\d+', entries)
+tmp = list(filter(lambda i: i > 1000 and i < 1000000000000000, list(map(int, tmp))))
+entries = []
+
+# extract entries
+for x in range(1, len(tmp), 2):
+    entries.append(tmp[x])
+print(entries)
+
 parser = {
     "First Name": entries[0],
     "Last Name": entries[1],
@@ -33,7 +39,9 @@ parser = {
     "ans1": entries[4],
     "ans2": entries[5]
 }
-url = "https://docs.google.com/forms/d/e/1FAIpQLSdBkt1jwgWZgpxhKUdQUM9S3Gn5qzXKJrtW0_HZbPAJaOeo5w/viewform?"
+url = "https://docs.google.com/forms/d/e/1FAIpQLSdBkt1jwgWZgpxhKUdQUM9S3Gn5qzXKJrtW0_HZbPAJaOeo5w/"
+if autosubmit: url += "formresponce?"
+else: url += "viewform?"
 
 for name, code in parser.items():
     url += f"entry.{code}={responce.get(name)}&"
