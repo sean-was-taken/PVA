2495f65c1c3673e84927a17af6414e021fad24b4
sean-was-taken <lelefacai@gmail.com>
2021-08-20 20:47:03 -0700

    update

--- pva_form.py
+++ pva_form.py
@@ -1,5 +1,4 @@
 import re, json
-# replace the following values with the information you wish to send to the form. 
 responce = json.load(open("entries.json", "r"))
 
 # use autosubmit = True for auto submit
@@ -18,16 +17,7 @@ tmp = list(filter(lambda i: i > 1000 and i < 1000000000000000, list(map(int, tmp
 entries = []
 for x in range(1, len(tmp), 2):
     entries.append(tmp[x])
-#print(entries)
 
-parser = {
-    "First Name": entries[0],
-    "Last Name": entries[1],
-    "Student Number": entries[2],
-    "Grade Level": entries[3],
-    "unwanted classes": entries[4],
-    "another school": entries[5]
-}
 url = "https://docs.google.com/forms/d/e/1FAIpQLSdBkt1jwgWZgpxhKUdQUM9S3Gn5qzXKJrtW0_HZbPAJaOeo5w/"
 if autosubmit: url += "formresponce?"
 else: url += "viewform?"
@@ -36,8 +26,6 @@ for x in range(len(entries)+1):
     if x < len(list(responce.values())):
         url += f"entry.{entries[x]}={list(responce.values())[x]}&"
     
-#for name, code in parser.items():
-#    url += f"entry.{code}={responce.get(name)}&"
 
 print(url)
 
